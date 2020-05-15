dda44e8a5d94b49e66bf39c4ed3f55e49ce103b9
Gautam Mishra <mishragautam96@gmail.com>
2020-05-15 16:59:37 +0530

    api updateTopic

--- src/main/java/me/mgautam/courseapi/Topic/TopicController.java
+++ src/main/java/me/mgautam/courseapi/Topic/TopicController.java
@@ -31,4 +31,9 @@ public class TopicController {
         topicService.addTopic(topic);
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+        topicService.updateTopic(id, topic);
+    }
+
 }
