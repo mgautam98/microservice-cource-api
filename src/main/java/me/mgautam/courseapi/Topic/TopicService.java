5251630cba0c40db8e71832045d264e777d94c6c
Gautam Mishra <mishragautam96@gmail.com>
2020-05-15 16:48:52 +0530

    api addTopic

--- src/main/java/me/mgautam/courseapi/Topic/TopicService.java
+++ src/main/java/me/mgautam/courseapi/Topic/TopicService.java
@@ -1,5 +1,6 @@
 package me.mgautam.courseapi.Topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,12 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
     
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
         new Topic("java", "JAVA language", "Java lang description"),
         new Topic("c++", "C++ language", "C++ lang description"),
         new Topic("sql", "SQL language", "SQL lang description"),
         new Topic("spring", "Spring framework", "Spring framework description")
-    );
+    ));
 
     public List<Topic> getAllTopics(){
         return topics;
@@ -21,6 +22,10 @@ public class TopicService {
 
     public Topic getTopic(String id){
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic){
+        topics.add(topic);
     }
 
 }
