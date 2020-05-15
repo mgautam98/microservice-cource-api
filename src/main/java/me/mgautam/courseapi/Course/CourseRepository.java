ed21f2089eea9d7a38ef6064d0242ec8cfc852c9
Gautam Mishra <mishragautam96@gmail.com>
2020-05-15 19:31:14 +0530

    add course apis

---
+++ src/main/java/me/mgautam/courseapi/Course/CourseRepository.java
@@ -0,0 +1,7 @@
+package me.mgautam.courseapi.Course;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+    
+}
--- src/main/java/me/mgautam/courseapi/Topic/TopicService.java
+++ src/main/java/me/mgautam/courseapi/Topic/TopicService.java
@@ -1,7 +1,6 @@
 package me.mgautam.courseapi.Topic;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +11,7 @@ public class TopicService {
     
     @Autowired
     private TopicRepository topicRepository;
-
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-        new Topic("java", "JAVA language", "Java lang description"),
-        new Topic("c++", "C++ language", "C++ lang description"),
-        new Topic("sql", "SQL language", "SQL lang description"),
-        new Topic("spring", "Spring framework", "Spring framework description")
-    ));
-
+    
     public List<Topic> getAllTopics(){
         List<Topic> topics= new ArrayList<>(); 
         topicRepository.findAll().forEach(topics::add);
