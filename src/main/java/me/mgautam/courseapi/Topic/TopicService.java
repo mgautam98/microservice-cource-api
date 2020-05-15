0a77397a17520442cd817d8ad854d9a72ad6a0ed
Gautam Mishra <mishragautam96@gmail.com>
2020-05-15 19:14:15 +0530

    add crud operations on db

--- src/main/java/me/mgautam/courseapi/Topic/TopicService.java
+++ src/main/java/me/mgautam/courseapi/Topic/TopicService.java
@@ -21,7 +21,6 @@ public class TopicService {
     ));
 
     public List<Topic> getAllTopics(){
-        // return topics;
         List<Topic> topics= new ArrayList<>(); 
         topicRepository.findAll().forEach(topics::add);
 
@@ -29,27 +28,18 @@ public class TopicService {
     }
 
     public Topic getTopic(String id){
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topicRepository.findById(id).orElse(null);
     }
 
     public void addTopic(Topic topic){
-        // topics.add(topic);
         topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic){
-
-        for (int i=0; i<topics.size(); i++){
-            Topic t = topics.get(i);
-
-            if(t.getId().equals(id)){
-                topics.set(i, topic);
-            }
-        }
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id){
-        topics.removeIf(t -> t.getId().equals(id));
-        return;
+        topicRepository.deleteById(id);
     }
 }
